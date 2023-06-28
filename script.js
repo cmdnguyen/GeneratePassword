d11f8a54935ec77470c72edc495068a181544608
cmdnguyen <cmdnguyen94@gmail.com>
2023-06-27 21:33:00 -0500

    updated createPasswordOptions functions and more in the script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-var characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var specialCharacters = "!@#$%^&*()";
-  //var password = generatePassword();
+const passwordOptions = {
+  numbers: "1234567890",
+  specialCharacters: "!@#$%&'()*+,^-./:;<=>?[]_`{~}|",
+  lowercaseCharacters: "abcdefghijklmnopqrstuvwxyz",
+  uppercaseCharacters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+};
+var generateBtn = document.querySelector("#generate")
+var password = "";
+var passwordLength = 0;
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
 
   createPasswordLength();
-  var passwordText = document.querySelector("#password");
+  
+    var passwordText = document.querySelector("#password");
 
     for (var i = 0; i <= passwordLength; i++){
       var randomNumber = Math.floor(Math.random * passwordLength);
@@ -15,30 +26,49 @@ function writePassword() {
     passwordText.value = password;
   }
     
-    function createPasswordLength() {
-      var passwordLength = prompt(
-        "How long would like the length of your password to be? (Must be 10 - 128 characters in length)"
-        );
+function createPasswordLength() {
+  var passwordLength = prompt(
+    "How long would like the length of your password to be? (Must be 10 - 128 characters in length)"
+      );
     
-      if (passwordLength < 10 || passwordLength > 128) {
-        alert(
-          "Please try again, password length must be 10 - 128 characters in length"
-          );
-        createPasswordLength();
+  if (passwordLength < 10 || passwordLength > 128) {
+    alert(
+      "Please try again, password length must be 10 - 128 characters in length"
+        );
+    createPasswordLength();
       }
-      return passwordLength;
+    return passwordLength;
 }
 
 function createPasswordOptions() {
-  var passwordLength = createPasswordLength();
+  createPasswordLength();
   
   var containsSpecialCharacters = confirm(
     "Click OK to confirm include special characters."
     );
+  if (containsSpecialCharacters){
+    password = password + passwordOptions.specialCharacters 
+  };
+
+  var containsNumbers = confirm(
+    "Click OK to confirm include numbers."
+    );
+    if (containsNumbers){
+      password = password + passwordOptions.numbers 
+    }; 
+
+  var containsLowercaseCharacters = confirm(
+    "Click OK to confirm include lowercase characters."
+     );
+    if (containsLowercaseCharacters){
+      password = password + passwordOptions.lowercaseCharacters 
+      };
+
+  var containsUppercaseCharacters = confirm(
+    "Click OK to confirm include uppercase characters."
+    );
+    if (containsUppercaseCharacters){
+      password = password + passwordOptions.uppercaseCharacters 
+      };
 }
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate")
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
